5cba1a8f815f3722bf672364b342869ad27b26e4
Linus Göldner <Linus@goeldner-family.de>
2024-02-10 12:38:32 +0100

    converting

--- src/lib.rs
+++ src/lib.rs
@@ -1,12 +1,12 @@
 #![allow(unused)]
 
+mod array_diff;
 mod highest_scoring_word;
 mod human_readable_time;
-mod array_diff;
-mod valid_paren;
 mod misc;
 mod more;
 mod unique_in_order;
+mod valid_paren;
 
 /// https://www.codewars.com/kata/5949481f86420f59480000e7/train/rust
 fn odd_or_even(numbers: Vec<i32>) -> String {
@@ -16,5 +16,3 @@ fn odd_or_even(numbers: Vec<i32>) -> String {
         _ => panic!("this doesn't happen!"),
     }
 }
-
-
--- src/unique_in_order.rs
+++ src/unique_in_order.rs
@@ -1,29 +1,29 @@
 //! https://www.codewars.com/kata/54e6533c92449cc251001667/train/rust
 fn unique_in_order<T>(sequence: T) -> Vec<T::Item>
 where
-	T: std::iter::IntoIterator,
-	T::Item: std::cmp::PartialEq + std::fmt::Debug,
+    T: std::iter::IntoIterator,
+    T::Item: std::cmp::PartialEq + std::fmt::Debug,
 {
-	let mut iterw = sequence.into_iter().peekable();
-	let mut res_vec = vec![];
-	while let Some(item) = iterw.next() {
-		if iterw.peek() != Some(&item) {
-			res_vec.push(item);
-		}
-	}
+    let mut iterw = sequence.into_iter().peekable();
+    let mut res_vec = vec![];
+    while let Some(item) = iterw.next() {
+        if iterw.peek() != Some(&item) {
+            res_vec.push(item);
+        }
+    }
 
-	res_vec
+    res_vec
 }
 
 #[cfg(test)]
 mod tests {
-	use super::*;
+    use super::*;
 
-	#[test]
-	fn sample_test() {
-		assert_eq!(
-			unique_in_order("AAAABBBCCDAABBB".chars()),
-			vec!['A', 'B', 'C', 'D', 'A', 'B']
-		);
-	}
+    #[test]
+    fn sample_test() {
+        assert_eq!(
+            unique_in_order("AAAABBBCCDAABBB".chars()),
+            vec!['A', 'B', 'C', 'D', 'A', 'B']
+        );
+    }
 }
